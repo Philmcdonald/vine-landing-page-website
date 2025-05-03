ba81a4e2cd16a5b3ec13d18baa07e8e1e38efc16
farouk <noblefarz7@gmail.com>
2025-05-03 17:11:25 +0100

    created a build

--- src/pages/whiteLabel/Index.jsx
+++ src/pages/whiteLabel/Index.jsx
@@ -1,27 +1,27 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
-import Hero from './components/Hero'
-import UseCase from './components/UseCase'
-import Branded from './components/Branded'
-import GetStarted from './components/GetStarted'
-import { Helmet } from 'react-helmet-async'
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Hero from "./components/Hero";
+import UseCase from "./components/UseCase";
+import Branded from "./components/Branded";
+import GetStarted from "./components/GetStarted";
+import { Helmet } from "react-helmet-async";
 
 const Index = () => {
   return (
     <Box>
-       <Helmet>
-                    <title>White Label | Vine Mobility</title>
-                    <meta
-                      name="White Label"
-                      content="Vine Mobility is transforming urban transport with smart solutions."
-                    />
-                  </Helmet>
+      <Helmet>
+        <title>White Label | Vine Mobility</title>
+        <meta
+          name="White Label"
+          content="Vine Mobility is transforming urban transport with smart solution."
+        />
+      </Helmet>
       <Hero />
       <UseCase />
       <Branded />
       <GetStarted />
     </Box>
-  )
-}
+  );
+};
 
-export default Index
+export default Index;
